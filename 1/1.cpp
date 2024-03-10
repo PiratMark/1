ed1f85ab9bb1010914b4a470e3ed3af949823f69
Компьютер <@DESKTOP-JR5U4KS>
2024-03-10 14:39:38 +0500

    Добавьте файлы проекта.

---
+++ 1/1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int x;
+
+	std::cout << "x=";
+	std::cin >> x;
+
+	if (x > 99999 && x < 1000000)
+	{
+		if (x / 100000 + x / 10000 % 10 + x / 1000 % 10 == x / 100 % 10 + x / 10 % 10 + x % 10)
+		std::cout << "счастливый";
+		else std::cout << "не счастливый";
+	}
+	else
+	{
+		std::cout << "ошибка";
+	}
+}
